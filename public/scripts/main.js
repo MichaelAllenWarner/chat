c78d86f572eeaee9910cf7c271697f2d7afea6fc
Michael Warner <44578578+MichaelAllenWarner@users.noreply.github.com>
2019-01-04 13:40:52 -0500

    basic chat app working

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,14 +1,37 @@
-function setUpWebSocket() {
-  const HOST = location.origin.replace(/^http/, 'ws');
-  const ws = new WebSocket(HOST);
+const HOST = location.origin.replace(/^http/, 'ws');
+const ws = new WebSocket(HOST);
+
+function setUpSubmitButton() {
+  document.querySelector('#submit-button').addEventListener('click', () => {
+    const outgoingMsg = document.querySelector('#message-input').value;
+    if (outgoingMsg) {
+      ws.send(outgoingMsg);
+    }
+    document.querySelector('#message-input').value = '';
+  });
+}
+
+function setUpMessageInput() {
+  document.querySelector('#message-input').addEventListener('keyup', function(event) {
+    if (event.key === 'Enter') {
+      document.querySelector('#submit-button').click();
+    }
+  });
+}
+
+function setUpMessageViewer () {
   ws.onmessage = (msg) => {
-    console.log('received message from server:');
-    console.log(msg);
+    let msgView = document.querySelector('#message-viewer').value;
+    document.querySelector('#message-viewer').value = `${msgView}\n${msg.data}`;
   }
 }
 
 if (document.readyState === 'loading') {
-  document.addEventListener('DOMContentLoaded', setUpWebSocket);
+  document.addEventListener('DOMContentLoaded', setUpSubmitButton);
+  document.addEventListener('DOMContentLoaded', setUpMessageInput);
+  document.addEventListener('DOMContentLoaded', setUpMessageViewer);
 } else {
-  setUpWebSocket();
+  setUpSubmitButton();
+  setUpMessageInput();
+  setUpMessageViewer();
 }
--- server.js
+++ server.js
@@ -2,23 +2,27 @@ const express = require('express');
 const http = require('http');
 const SocketServer = require('ws').Server;
 
-const port = process.env.PORT || 3000;
-const app = express();
-app.get('/', (req, res) => {
-  res.sendFile(`${__dirname}/public/index.html`)
-});
-app.use(express.static('public'));
-// app.listen(port);
+const app = express()
+  .get('/', (req, res) => {
+    res.sendFile(`${__dirname}/public/index.html`)
+  })
+  .use(express.static('public'));
 
 const httpServer = http.createServer(app);
 
 const wss = new SocketServer({ 'server': httpServer });
 wss.on('connection', (ws) => {
   console.log('client connected');
-  ws.send('msg to client: connection established on server');
+  ws.on('message', (msgFromSomeUser) => {
+    console.log('received msg from client: ');
+    console.log(msgFromSomeUser);
+    wss.clients.forEach(client => {
+      client.send(msgFromSomeUser);
+    });
+  });
   ws.on('close', () => {
     console.log('client disconnected');
   });
 });
 
-httpServer.listen(port);
+httpServer.listen(process.env.PORT || 3000);
