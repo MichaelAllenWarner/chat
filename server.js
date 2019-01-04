e1620579e35b219659dc89988c5ba1f4203f73eb
Michael Warner <44578578+MichaelAllenWarner@users.noreply.github.com>
2019-01-04 11:14:33 -0500

    First commit. No function or content yet.

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.sendFile(`${__dirname}/public/index.html`)
+});
+
+app.use(express.static('public'));
+
+app.listen(process.env.PORT || 3000);
