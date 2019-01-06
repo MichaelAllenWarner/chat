c0e76925b53b28593542fc72ca1f70430bd7e581
Michael Warner <44578578+MichaelAllenWarner@users.noreply.github.com>
2019-01-06 12:24:21 -0500

    message viewer now stays scrolled at bottom as new messages come in

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,16 +1,29 @@
 const HOST = location.origin.replace(/^http/, 'ws');
 const ws = new WebSocket(HOST);
 
+if (document.readyState === 'loading') {
+  document.addEventListener('DOMContentLoaded', setUpSubmitButton);
+  document.addEventListener('DOMContentLoaded', setUpMessageInput);
+  document.addEventListener('DOMContentLoaded', setUpWebSocketMsgReception);
+} else {
+  setUpSubmitButton();
+  setUpMessageInput();
+  setUpWebSocketMsgReception();
+}
+
 function setUpSubmitButton() {
   document.querySelector('#submit-button').addEventListener('click', () => {
-    const outgoingMsgObj ={
+    const outgoingMsgObj = {
+      type: 'text',
       user: document.querySelector('#username').value || 'An anonymous user',
       time: Date.now(),
       text: document.querySelector('#message-input').value
     };
+
     if (outgoingMsgObj.text) {
       ws.send(JSON.stringify(outgoingMsgObj));
     }
+
     document.querySelector('#message-input').value = '';
   });
 }
@@ -23,30 +36,38 @@ function setUpMessageInput() {
   });
 }
 
-function setUpMessageViewer () {
+function setUpWebSocketMsgReception () {
   ws.onmessage = (incomingMsgObj) => {
     const msgData = JSON.parse(incomingMsgObj.data);
-    const user = msgData.user;
-    const time = new Date(msgData.time);
-    const text = msgData.text;
-    const newMsg = document.createElement ('p');
-    newMsg.textContent = `${user} said: ${text}`;
-    newMsg.setAttribute('data-time', time);
-    const ownUsername = document.querySelector('#username').value;
-    const senderClass = (user === ownUsername) ? 'own-message' : 'other-message';
-    newMsg.classList.add(senderClass);
-    const messageViewer = document.querySelector('#message-viewer');
-    messageViewer.appendChild(newMsg);
-    console.log(incomingMsgObj);
-  }
-}
 
-if (document.readyState === 'loading') {
-  document.addEventListener('DOMContentLoaded', setUpSubmitButton);
-  document.addEventListener('DOMContentLoaded', setUpMessageInput);
-  document.addEventListener('DOMContentLoaded', setUpMessageViewer);
-} else {
-  setUpSubmitButton();
-  setUpMessageInput();
-  setUpMessageViewer();
+    if (msgData.type = 'text') {
+      processNewTextMsg(msgData);
+    }
+
+    function processNewTextMsg(msgData) {
+      const viewer = document.querySelector('#message-viewer');
+      const isScrolledDown = (viewer.scrollHeight - viewer.scrollTop <= viewer.clientHeight + 5);
+
+      const user = msgData.user;
+      const time = new Date(msgData.time);
+      const text = msgData.text;
+
+      const ownUsername = document.querySelector('#username').value;
+      // FIXME: need foolproof way of distinguishing own message from other message
+      const msgUserClass = (user === ownUsername) ? 'own-message' : 'other-message';
+
+      const newMsg = document.createElement ('p');
+      newMsg.textContent = `${user} said: ${text}`;
+      newMsg.setAttribute('data-time', time);
+      newMsg.classList.add(msgUserClass);
+      viewer.appendChild(newMsg);
+
+      // scroll down only if already nearly scrolled down
+      if (isScrolledDown) {
+        viewer.scrollTop = viewer.scrollHeight - viewer.clientHeight; 
+      }
+
+      console.log(incomingMsgObj);
+    }
+  }
 }
