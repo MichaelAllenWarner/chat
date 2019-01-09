930e205955d04e5e37413e14589078012f6188db
Michael Warner <44578578+MichaelAllenWarner@users.noreply.github.com>
2019-01-09 09:53:12 -0500

    working on passing globals into functions

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -3,122 +3,128 @@ const ws = new WebSocket(HOST);
 const ids = {}; // one publicid, one privateid, server will send
 
 if (document.readyState === 'loading') {
-  document.addEventListener('DOMContentLoaded', setUpMessageInput);
-  document.addEventListener('DOMContentLoaded', setUpUsernameInput);
-  document.addEventListener('DOMContentLoaded', setUpWebSocketMsgReception);
+  document.addEventListener('DOMContentLoaded', setUpMessageInput(ws, ids));
+  document.addEventListener('DOMContentLoaded', setUpUsernameInput(ws, ids));
+  document.addEventListener('DOMContentLoaded', setUpWebSocketMsgReception(ws, ids));
 } else {
-  setUpMessageInput();
-  setUpUsernameInput();
-  setUpWebSocketMsgReception();
+  setUpMessageInput(ws, ids);
+  setUpUsernameInput(ws, ids);
+  setUpWebSocketMsgReception(ws, ids);
 }
 
-function setUpMessageInput() {
-  const messageInput = document.querySelector('#message-input');
-  const usernameInput = document.querySelector('#username-input');
-  messageInput.addEventListener('keydown', function(event) {
-    if (event.key === 'Enter' && this.value) {
-      const outgoingMsgObj = {
-        privateid: ids.privateid,
-        publicid: ids.publicid,
-        username: usernameInput.value,
-        time: Date.now(),
-        text: this.value
-      };
-      ws.send(JSON.stringify(outgoingMsgObj));
-      this.value = '';
-      // this.blur(); want this for mobile maybe so that keyboard disappears
-    }
-  });
-}
-
-function setUpUsernameInput() {
-  document.querySelector('#username-input').addEventListener('keydown', function(event) {
-    if (event.key === 'Enter') {
-      const errorMessage = document.querySelector('#error-message');
-      errorMessage.textContent = 'Submit a message to update your username.';
-    }
-  });
-}
-
-function setUpWebSocketMsgReception () {
-  ws.onmessage = (incomingMsgObj) => {
-    console.log(incomingMsgObj);
-    const msgData = JSON.parse(incomingMsgObj.data);
-
-    switch (msgData.type) {
-      case 'error':
-        const errorMessage = document.querySelector('#error-message');
-        errorMessage.textContent = msgData.error;
-        break;
-      case 'ownids':
-        ids.publicid = msgData.yourPublicid;
-        ids.privateid = msgData.yourPrivateid;
-        break;
-      case 'users':
-        updateUsernamesList(msgData.users, ids.publicid);
-        break;
-      case 'text':
-        processNewTextMsg(msgData, ids.publicid);
-        break;
-    }
-
-    function updateUsernamesList(usersObj, ownPublicid) {
-      const usernamesList = document.querySelector('#usernames-list');
-      while (usernamesList.firstChild) {
-        usernamesList.removeChild(usernamesList.firstChild);
+function setUpMessageInput(ws, ids) {
+  return () => {
+    const messageInput = document.querySelector('#message-input');
+    const usernameInput = document.querySelector('#username-input');
+    messageInput.addEventListener('keydown', function(event) {
+      if (event.key === 'Enter' && this.value) {
+        const outgoingMsgObj = {
+          privateid: ids.privateid,
+          publicid: ids.publicid,
+          username: usernameInput.value,
+          time: Date.now(),
+          text: this.value
+        };
+        ws.send(JSON.stringify(outgoingMsgObj));
+        this.value = '';
+        // this.blur(); want this for mobile maybe so that keyboard disappears
       }
-      const ownUserItem = document.createElement('li');
-      ownUserItem.id = 'own-user';
-      ownUserItem.setAttribute('data-publicid', ownPublicid)
-      usernamesList.appendChild(ownUserItem);
-      for (const [publicid, username] of Object.entries(usersObj)) {
-        if (publicid === ownPublicid) {
-          ownUserItem.textContent = (username) ? `${username} (You)` : '(You)';
-        } else {
-          const usernameItem = document.createElement('li');
-          usernameItem.textContent = username || 'An anonymous user';
-          usernameItem.setAttribute('data-publicid', publicid);
-          usernamesList.appendChild(usernameItem);
+    });
+  }
+}
+
+function setUpUsernameInput(ws, ids) {
+  return () => {
+    document.querySelector('#username-input').addEventListener('keydown', function(event) {
+      if (event.key === 'Enter') {
+        const errorMessage = document.querySelector('#error-message');
+        errorMessage.textContent = 'Submit a message to update your username.';
+      }
+    });
+  }
+}
+
+function setUpWebSocketMsgReception (ws, ids) {
+  return () => {
+    ws.onmessage = (incomingMsgObj) => {
+      console.log(incomingMsgObj);
+      const msgData = JSON.parse(incomingMsgObj.data);
+
+      switch (msgData.type) {
+        case 'error':
+          const errorMessage = document.querySelector('#error-message');
+          errorMessage.textContent = msgData.error;
+          break;
+        case 'ownids':
+          ids.publicid = msgData.yourPublicid;
+          ids.privateid = msgData.yourPrivateid;
+          break;
+        case 'users':
+          updateUsernamesList(msgData.users, ids.publicid);
+          break;
+        case 'text':
+          processNewTextMsg(msgData, ids.publicid);
+          break;
+      }
+
+      function updateUsernamesList(usersObj, ownPublicid) {
+        const usernamesList = document.querySelector('#usernames-list');
+        while (usernamesList.firstChild) {
+          usernamesList.removeChild(usernamesList.firstChild);
+        }
+        const ownUserItem = document.createElement('li');
+        ownUserItem.id = 'own-user';
+        ownUserItem.setAttribute('data-publicid', ownPublicid)
+        usernamesList.appendChild(ownUserItem);
+        for (const [publicid, username] of Object.entries(usersObj)) {
+          if (publicid === ownPublicid) {
+            ownUserItem.textContent = (username) ? `${username} (You)` : '(You)';
+          } else {
+            const usernameItem = document.createElement('li');
+            usernameItem.textContent = username || 'An anonymous user';
+            usernameItem.setAttribute('data-publicid', publicid);
+            usernamesList.appendChild(usernameItem);
+          }
         }
       }
-    }
 
-    function processNewTextMsg(msgData, ownPublicid) {
-      const viewer = document.querySelector('#messages-viewer');
-      const wasScrolledDown = (viewer.scrollHeight - viewer.scrollTop <= viewer.clientHeight + 5);
+      function processNewTextMsg(msgData, ownPublicid) {
+        const viewer = document.querySelector('#messages-viewer');
+        const wasScrolledDown = (viewer.scrollHeight - viewer.scrollTop <= viewer.clientHeight + 5);
 
-      const publicid = msgData.publicid;
-      const username = (publicid === ownPublicid) ? 'You'
-      : (!msgData.username) ? 'An anonymous user'
-      : msgData.username;
-      const time = new Date(msgData.time);
-      const text = msgData.text;
+        const publicid = msgData.publicid;
+        const username = (publicid === ownPublicid) ? 'You'
+        : (!msgData.username) ? 'An anonymous user'
+        : msgData.username;
+        const time = new Date(msgData.time);
+        const text = msgData.text;
 
-      const newMsg = document.createElement('p');
-      const msgUserClass = (publicid === ownPublicid) ? 'own-message' : 'other-message';
-      newMsg.classList.add(msgUserClass);
-      newMsg.setAttribute('data-time', time);
+        const newMsg = document.createElement('p');
+        const msgUserClass = (publicid === ownPublicid) ? 'own-message' : 'other-message';
+        newMsg.classList.add(msgUserClass);
+        newMsg.setAttribute('data-time', time);
 
-      const usernameSpan = document.createElement('span');
-      usernameSpan.textContent = `${username}: `;
-      usernameSpan.classList.add('username-prefix');
-      newMsg.appendChild(usernameSpan);
+        const usernameSpan = document.createElement('span');
+        usernameSpan.textContent = `${username}: `;
+        usernameSpan.classList.add('username-prefix');
+        newMsg.appendChild(usernameSpan);
 
-      const textNode = document.createTextNode(text);
-      newMsg.appendChild(textNode);
+        const textNode = document.createTextNode(text);
+        newMsg.appendChild(textNode);
 
-      viewer.appendChild(newMsg);
+        viewer.appendChild(newMsg);
 
-      // scroll down only if already was nearly scrolled down
-      if (wasScrolledDown) {
-        viewer.scrollTop = viewer.scrollHeight - viewer.clientHeight; 
+        // scroll down only if already was nearly scrolled down
+        if (wasScrolledDown) {
+          viewer.scrollTop = viewer.scrollHeight - viewer.clientHeight; 
+        }
+
+        if (publicid === ownPublicid) {
+          const errorP = document.querySelector('#error-message');
+          errorP.textContent = '';
+        }
+
       }
-
-      if (publicid === ownPublicid) {
-        const errorP = document.querySelector('#error-message');
-        errorP.textContent = '';
-      }
-
     }
   }
 }
--- server.js
+++ server.js
@@ -23,63 +23,67 @@ wss.broadcast = data => {
 
 const publicUsersObj = { type: 'users', users: {} };
 
-wss.on('connection', ws => {
-  ws.publicid = uniqid();
-  ws.privateid = bcrypt.hashSync(uniqid(), 0);
-  publicUsersObj.users[ws.publicid] = '';
-  ws.send(JSON.stringify({
-    type: 'ownids',
-    yourPublicid: ws.publicid,
-    yourPrivateid: ws.privateid
-  }));
-  console.log('client connected, publicid: ' + ws.publicid);
-  console.log(publicUsersObj);
-  wss.broadcast(JSON.stringify(publicUsersObj));
+wss.on('connection', wssOnConnectionCallback(publicUsersObj));
 
-  ws.on('message', msgString => {
-    const msgObj = JSON.parse(msgString);
-    console.log('received msg from client:');
-    console.log(msgObj);
-    // verify integrity of msgObj, send error message if there's a problem
-    if (typeof msgObj !== 'object'
-        || 'privateid' in msgObj === false
-        || 'publicid' in msgObj === false
-        || 'username' in msgObj === false
-        || 'time' in msgObj === false
-        || 'text' in msgObj === false
-        || msgObj.privateid !== ws.privateid
-        || msgObj.publicid !== ws.publicid
-        || typeof msgObj.text !== 'string') {
-      ws.send(JSON.stringify({
-        type: 'error',
-        error: 'Problem with submitted data. Message delivery failed. You may need to refresh page.'
-      }));
-      return;
-    }
-    // update username if necessary and tell all clients
-    if (msgObj.username !== publicUsersObj.users[ws.publicid]) {
-      // send error message if new username is already taken
-      if (Object.values(publicUsersObj.users).includes(msgObj.username)) {
+function wssOnConnectionCallback(publicUsersObj) {
+  return function wssOnConnectionHandler(ws) => {
+    ws.publicid = uniqid();
+    ws.privateid = bcrypt.hashSync(uniqid(), 0);
+    publicUsersObj.users[ws.publicid] = '';
+    ws.send(JSON.stringify({
+      type: 'ownids',
+      yourPublicid: ws.publicid,
+      yourPrivateid: ws.privateid
+    }));
+    console.log('client connected, publicid: ' + ws.publicid);
+    console.log(publicUsersObj);
+    wss.broadcast(JSON.stringify(publicUsersObj));
+
+    ws.on('message', msgString => {
+      const msgObj = JSON.parse(msgString);
+      console.log('received msg from client:');
+      console.log(msgObj);
+      // verify integrity of msgObj, send error message if there's a problem
+      if (typeof msgObj !== 'object'
+          || 'privateid' in msgObj === false
+          || 'publicid' in msgObj === false
+          || 'username' in msgObj === false
+          || 'time' in msgObj === false
+          || 'text' in msgObj === false
+          || msgObj.privateid !== ws.privateid
+          || msgObj.publicid !== ws.publicid
+          || typeof msgObj.text !== 'string') {
         ws.send(JSON.stringify({
           type: 'error',
-          error: 'That username is taken. Try again with a unique handle.'
+          error: 'Problem with submitted data. Message delivery failed. You may need to refresh page.'
         }));
         return;
       }
-      publicUsersObj.users[ws.publicid] = msgObj.username;
-      wss.broadcast(JSON.stringify(publicUsersObj));
-    }
-    delete msgObj.privateid;
-    msgObj.type = 'text';
-    wss.broadcast(JSON.stringify(msgObj));
-  });
+      // update username if necessary and tell all clients
+      if (msgObj.username !== publicUsersObj.users[ws.publicid]) {
+        // send error message if new username is already taken
+        if (Object.values(publicUsersObj.users).includes(msgObj.username)) {
+          ws.send(JSON.stringify({
+            type: 'error',
+            error: 'That username is taken. Try again with a unique handle.'
+          }));
+          return;
+        }
+        publicUsersObj.users[ws.publicid] = msgObj.username;
+        wss.broadcast(JSON.stringify(publicUsersObj));
+      }
+      delete msgObj.privateid;
+      msgObj.type = 'text';
+      wss.broadcast(JSON.stringify(msgObj));
+    });
 
-  ws.on('close', () => {
-    console.log('client disconnected, publicid: ' + ws.publicid);
-    delete publicUsersObj.users[ws.publicid];
-    console.log(publicUsersObj);
-    wss.broadcast(JSON.stringify(publicUsersObj));
-  });
-});
+    ws.on('close', () => {
+      console.log('client disconnected, publicid: ' + ws.publicid);
+      delete publicUsersObj.users[ws.publicid];
+      console.log(publicUsersObj);
+      wss.broadcast(JSON.stringify(publicUsersObj));
+    });
+  }
+}
 
 httpServer.listen(process.env.PORT || 3000);
