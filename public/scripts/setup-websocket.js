2f8c986e1453e33d1c5c0dda785384426ef5cb2a
Michael Warner <44578578+MichaelAllenWarner@users.noreply.github.com>
2019-01-09 22:31:56 -0500

    still working on mobile

--- public/scripts/setup-websocket.js
+++ public/scripts/setup-websocket.js
@@ -1,8 +1,21 @@
 const HOST = location.origin.replace(/^http/, 'ws');
 const ws = new WebSocket(HOST);
 
-function scrollPastAddressBar() {
-  window.scrollTo(0, 1);
+function hideAddressBar() {
+  if (!window.location.hash) {
+    if (document.height < window.outerHeight) {
+      document.body.style.height = (window.outerHeight + 50) + 'px';
+    }
+
+    setTimeout(function() {
+      window.scrollTo(0, 1);
+    }, 50);
+  }
 }
 
-window.onload = scrollPastAddressBar;
+window.addEventListener("load", () => {
+  if (!window.pageYOffset) {
+    hideAddressBar();
+  }
+});
+window.addEventListener("orientationchange", hideAddressBar);
