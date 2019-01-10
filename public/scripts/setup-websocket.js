e6101087c2aa9c3f73119432b5d67c0ef5532796
Michael Warner <44578578+MichaelAllenWarner@users.noreply.github.com>
2019-01-09 22:25:47 -0500

    trying to scroll past address bar on mobile for full screen UX

--- public/scripts/setup-websocket.js
+++ public/scripts/setup-websocket.js
@@ -1,2 +1,8 @@
 const HOST = location.origin.replace(/^http/, 'ws');
 const ws = new WebSocket(HOST);
+
+function scrollPastAddressBar() {
+  window.scrollTo(0, 1);
+}
+
+window.onload = scrollPastAddressBar;
