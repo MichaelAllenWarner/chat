e57dbc3c0e03aa65a6385b9332c4255811db2d98
Michael Warner <44578578+MichaelAllenWarner@users.noreply.github.com>
2019-02-05 16:19:42 -0500

    improve src folder structure

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,18 +1,21 @@
 import { ids } from './ids';
-import { ws } from './ws-connect';
-import setUpWSSending from './ws-sending';
-import setUpWSReceiving from './ws-receiving';
-import setUpResponsiveLayout from './responsive-layout';
-import setUpMenuToggle from './menu-toggle';
-import setUpDarkModeToggle from './dark-mode-toggle';
+import { ws } from './websocket/ws-connect';
+import setUpWSSending from './websocket/ws-sending';
+import setUpWSReceiving from './websocket/ws-receiving';
+import setUpResponsiveLayout from './ui-ux/responsive-layout';
+import setUpMenuToggle from './ui-ux/menu-toggle';
+import setUpDarkModeToggle from './ui-ux/dark-mode-toggle';
+
+// WebSocket
+setUpWSSending(ids, ws);
+setUpWSReceiving(ids, ws);
+
+// UI/UX
+setUpResponsiveLayout();
+setUpMenuToggle();
+setUpDarkModeToggle();
 
 // because of Heroku 55-second timeout:
 // ws.onclose = event => {
 //   alert('Server idles after 1 minute of inactivity. Refresh page to reconnect.');
 // }
-
-setUpWSSending(ids, ws);
-setUpWSReceiving(ids, ws);
-setUpResponsiveLayout();
-setUpMenuToggle();
-setUpDarkModeToggle();
